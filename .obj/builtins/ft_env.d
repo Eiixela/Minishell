ec2e14d150f9d82c8534b94649b9f2126587058d
Alexie Julien <aljulien@z1r4p3.42lyon.fr>
2024-08-13 15:31:04 +0200

    push after merged

---
+++ .obj/builtins/ft_env.d
@@ -0,0 +1,6 @@
+.obj/builtins/ft_env.o: srcs/builtins/ft_env.c inc/minishell.h \
+  inc/../libft/libft.h
+
+inc/minishell.h:
+
+inc/../libft/libft.h:
---
+++ .obj/parsing/utils/redirection_utils.d
@@ -0,0 +1,7 @@
+.obj/parsing/utils/redirection_utils.o: \
+  srcs/parsing/utils/redirection_utils.c inc/minishell.h \
+  inc/../libft/libft.h
+
+inc/minishell.h:
+
+inc/../libft/libft.h:
