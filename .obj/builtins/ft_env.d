cd07fd5dde2b707579c5d566fb2aa25565d6e33a
Alexie Julien <aljulien@z1r4p3.42lyon.fr>
2024-08-13 16:33:22 +0200

    cleaned the fprintf in exit

--- .obj/builtins/ft_env.d
+++
@@ -1,6 +0,0 @@
-.obj/builtins/ft_env.o: srcs/builtins/ft_env.c inc/minishell.h \
-  inc/../libft/libft.h
-
-inc/minishell.h:
-
-inc/../libft/libft.h:
--- .obj/main/minishell.d
+++
@@ -1,6 +0,0 @@
-.obj/main/minishell.o: srcs/main/minishell.c inc/minishell.h \
-  inc/../libft/libft.h
-
-inc/minishell.h:
-
-inc/../libft/libft.h:
