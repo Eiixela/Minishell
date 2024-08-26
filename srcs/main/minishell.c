dea36c8d4ebe463a39599ce19c52cda3460a4608
Alexie Julien <aljulien@z1r4p1.42lyon.fr>
2024-08-26 15:28:09 +0200

    fixed leaks in main t_env

--- srcs/main/minishell.c
+++ srcs/main/minishell.c
@@ -6,7 +6,7 @@
 /*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/10 17:45:51 by aljulien          #+#    #+#             */
-/*   Updated: 2024/08/26 15:09:20 by aljulien         ###   ########.fr       */
+/*   Updated: 2024/08/26 15:27:17 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ int	main(int ac, char **av, char **envp)
 		sigend();
 		str = readline("aljulien@z3r8p5:~/goinfre/minishell$ ");
 		if (!str)
-			return (cleanup(&line), 0);
+			return (cleanup(&line), free_env(env), 0);
 		if (str && *str)
 		{
 			add_history(str);
