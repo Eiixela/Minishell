e37f089c44364a54ba9b79ebf86eced62e0ab129
Alexie Julien <aljulien@z1r3p1.42lyon.fr>
2024-09-03 09:22:19 +0200

    fixed the cat command in cat | ls and so forth, but also in classic command where the cat command doesn;t reauire the user the press enter to get the prompt back, just like in bash :))))

--- srcs/exec/pipex.c
+++ srcs/exec/pipex.c
@@ -6,7 +6,7 @@
 /*   By: aljulien <aljulien@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 11:48:55 by aljulien          #+#    #+#             */
-/*   Updated: 2024/09/03 09:05:21 by aljulien         ###   ########.fr       */
+/*   Updated: 2024/09/03 09:21:07 by aljulien         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,89 +43,30 @@ int create_process(t_env *env, t_pipe *pipe, int input_fd, int output_fd, t_line
             else if (redir_result == 2)
                 exit(pipe->ret_val);
         }
-        if (strcmp(pipe->arg[0], "cat") == 0 && pipe->arg[1] == NULL && isatty(STDIN_FILENO))
-        {
-            builtin_result = execute_builtins(env, pipe, line);
-            exit(builtin_result);  // Exit with the return value of the builtin
-        }
-
-        else
-        {
-            if (execute_cmd(env, pipe, line))
-                exit(pipe->ret_val);
-        }
+        execute_cmd(env, pipe, line);
+        exit(pipe->ret_val);
     }
     return (pid);
 }
+
+
 
 int _call_childs(t_env *env, t_line *line)
 {
     int pipe_fd[2], input_fd = 0, status;
     t_pipe *current = line->pipe;
     pid_t pid, last_pid = 0;
+    int cat_count = 0;
 
-	(void)last_pid;
-    while (current != NULL)
-    {
-        if (current->next != NULL)
-        {
-            if (pipe(pipe_fd) == -1)
-                return (perror("pipe"), 0);
-
-            pid = create_process(env, current, input_fd, pipe_fd[1], line);
-            close(pipe_fd[1]);
-            if (input_fd != 0)
-                close(input_fd);
-            input_fd = pipe_fd[0];
-        }
-        else
-        {
-            pid = create_process(env, current, input_fd, 1, line);
-            if (input_fd != 0)
-                close(input_fd);
-        }
-        last_pid = pid;
-        current = current->next;
-    }
-    pid_t wpid;
-    sigend();
-    while ((wpid = wait(&status)) > 0)
-    {
-        if (wpid == last_pid) {
-            handle_exit_status_child(line, status, &quit_message_printed);
-        }
-    }
-    return (1);
-}
-
-/*int _call_childs(t_env *env, t_line *line)
-{
-    int     pipe_fd[2];
-    int     input_fd;
-    t_pipe  *current;
-    pid_t   pid;
-    pid_t   last_pid = 0;
-    int     quit_message_printed = 0;
-    int     builtin_result;
-    int     status;
-    int     cat_count = 0;
-
-    current = line->pipe;
-    input_fd = 0;
-
-    builtin_result = parse_and_execute_solo_builtins(env, line);
-    if (builtin_result == 2)
-        return (1);
-    if (builtin_result == 0)
-        return (0);
-
+    (void)last_pid;
     // Count initial cat commands
-    while (current && strcmp(current->arg[0], "cat") == 0 && current->arg[1] == NULL)
+    while (current && strcmp(current->arg[0], "cat") == 0 && current->arg[1] == NULL && !current->redir)
     {
         cat_count++;
         current = current->next;
     }
     current = line->pipe;
+
     while (current != NULL)
     {
         if (current->next != NULL)
@@ -142,11 +83,8 @@ int _call_childs(t_env *env, t_line *line)
                     dup2(pipe_fd[1], STDOUT_FILENO);
                     close(pipe_fd[1]);
                     char buf[1024];
-                    ssize_t n;
-                    if ((n = read(STDIN_FILENO, buf, sizeof(buf))) > 0)
-                    {
-                        write(STDOUT_FILENO, buf, n);
-                    }
+                    if (read(STDIN_FILENO, buf, sizeof(buf)) > 0)
+                        write(STDOUT_FILENO, buf, strlen(buf));
                     exit(0);
                 }
                 cat_count--;
@@ -166,19 +104,22 @@ int _call_childs(t_env *env, t_line *line)
             if (input_fd != 0)
                 close(input_fd);
         }
-        if (current->next == NULL)
-            last_pid = pid;
+        last_pid = pid;
         current = current->next;
     }
-    pid_t wpid;
-    sigend();
-    while ((wpid = wait(&status)) > 0)
+
+    while (wait(&status) > 0);
+    
+    // Handle remaining cat inputs
+    while (cat_count > 0)
     {
-        if (wpid == last_pid)
-            handle_exit_status_child(line, status, &quit_message_printed);
+        char buf[2];
+        read(STDIN_FILENO, buf, 1);
+        cat_count--;
     }
+
     return (1);
-}*/
+}
 
 int	pipex(t_env *env, t_line *line, int *status)
 {
